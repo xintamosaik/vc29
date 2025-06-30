6694d8cd15c9af4dccb61ef8ddd6cd9620683157
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-30 11:29:55 +0200

    add web "hello world"

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
-func main () {
-// We will do routes here and then serve the app 
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	fmt.Println("Starting server on :3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
