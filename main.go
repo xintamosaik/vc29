c9ab68b607c137685bcf7078095841e6aa19d0f2
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-30 12:55:32 +0200

    add index.html template and update main.go to serve dynamic content

--- main.go
+++ main.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
+const port = ":3000"
+
+type PageData struct {
+	Title string
+	Content string
+}
 
 func main() {
+	tmpl := template.Must(template.ParseFiles("index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
+		data := PageData{
+			Title:   "Hello World",
+			Content: "Welcome to the world of web development. Enjoy coding!",
+		}
+		tmpl.Execute(w, data)
 	})
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	fmt.Println("Starting server on :3000")
-	if err := http.ListenAndServe(":3000", nil); err != nil {
+	fmt.Println("Starting server on http://localhost" + port)
+	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
 }
