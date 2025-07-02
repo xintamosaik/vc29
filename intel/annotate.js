beac920d85a7127fb671f5bebe6a59675a954cdd
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-07-02 14:17:36 +0200

    It works but it does not feel right

--- intel/annotate.js
+++ intel/annotate.js
@@ -6,21 +6,32 @@ export default function AnnotationScript(event) {
     console.log(range)
 
     const start = range.startContainer
-    const startSpan = start.parentElement;
-    const startWord = startSpan.dataset.word;
+    const startNodeName = start.nodeName.toLowerCase();
+    
+    const startSpan = startNodeName === "#text" ? start.parentElement : start;
+    const startWord = startNodeName === "#text" ? startSpan.dataset.word : 0;
 
-    const startParagraph = startSpan.parentElement;
+    const startParagraph = startNodeName === "#text" ? startSpan.parentElement : start;
     const startParagraphIndex = startParagraph.dataset.paragraph;
 
     const end = range.endContainer
-    const endSpan = end.parentElement;
-    const endParagraph = endSpan.parentElement;
+    const endNodeName = end.nodeName.toLowerCase();
 
-    const endWord = endSpan.dataset.word;
+    const endSpan = endNodeName === "#text" ? end.parentElement : end;
+    const endWord = endNodeName === "#text" ? endSpan.dataset.word : 0;
+
+    const endParagraph = endNodeName === "#text" ? endSpan.parentElement : end;
+
+    
     const endParagraphIndex = endParagraph.dataset.paragraph;
 
-    console.log("Selected text from paragraph", startParagraphIndex, "word", startWord, "to paragraph", endParagraphIndex, "word", endWord);
+    const selectedText = range.toString().trim();
+    if (!selectedText) return;
 
+    console.log("Selected text from paragraph", startParagraphIndex, "word", startWord, "to paragraph", endParagraphIndex, "word", endWord);
+    const info = document.querySelector("#info");
+    const infoText = `Selected "${selectedText}" from paragraph ${startParagraphIndex} word ${startWord} to paragraph ${endParagraphIndex} word ${endWord}`;
+    info.textContent = infoText;
     const popover = document.querySelector("#send_annotation");
 
     popover.showPopover();
