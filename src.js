fb5105ce512ec884f65c5fef80146e8bf48196d6
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-07-04 18:52:05 +0200

    fix: clarify comment for global mouseup handler in src.js

--- src.js
+++ src.js
@@ -1,5 +1,5 @@
 import './src.css';
 import AnnotationScript from './intel/annotate.js';
 
-// Make the function globally available for the templ onmouseup handler
+// Make the function globally available for the templ onmouseup handler (neeed for esbuild to not treeshake it)
 window.handleAnnotateMouseUp = AnnotationScript;
