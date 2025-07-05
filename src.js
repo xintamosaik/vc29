a7374c18b0a73969d6b69bb4cf39aa7b19527721
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-07-05 19:17:17 +0200

    refactor navigation initialization and active link handling for improved functionality

--- src.js
+++ src.js
@@ -3,3 +3,20 @@ import AnnotationScript from './pages.annotate.js';
 
 // Make the function globally available for the templ onmouseup handler (neeed for esbuild to not treeshake it)
 window.handleAnnotateMouseUp = AnnotationScript;
+(function initNavigation() {
+    const navigation = window.main_navigation;
+    const links = navigation.querySelectorAll('a');
+    function setActive(event) {
+        if (event.target.tagName !== 'A') {
+            return;
+        }
+        links.forEach(link => {
+            if (link === event.target) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    }
+    navigation.addEventListener('click', setActive);
+})();   
