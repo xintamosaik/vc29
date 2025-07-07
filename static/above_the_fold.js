4d05fdd64fc225ad87e4008a60b562f4bd010e81
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-07-07 20:47:52 +0200

    refactor: simplify navigation event handling and remove debug logs

--- static/above_the_fold.js
+++ static/above_the_fold.js
@@ -3,36 +3,27 @@
     const links = navigation.querySelectorAll('a');
 
     function navigate(event) {
-        const sections = document.querySelectorAll('section.main');
-        if (event.target.tagName !== 'A') {
+        const clicked = event.target
+        if (clicked.tagName !== 'A') {
             return;
         }
 
-        const target = event.target.dataset.reference;
-        console.log(`Navigating to: ${target}`);
-
+        const sections = document.querySelectorAll('section.main');
         sections.forEach(section => {
-            console.log(`Checking section: ${section.id}`);
-            if (section.id === target) {
-                console.log(`Showing section: ${section.id}`);
+            if (section.id === clicked.dataset.reference) {
                 section.classList.remove('hidden');
             } else {
-                console.log(`Hiding section: ${section.id}`);
                 section.classList.add('hidden');
             }
         });
 
-
-
         links.forEach(link => {
-            if (link === event.target) {
+            if (link === clicked) {
                 link.classList.add('nav-active')
             } else {
                 link.classList.remove('nav-active');
             }
         });
-
-
     }
     navigation.addEventListener('click', navigate);
 })();   
