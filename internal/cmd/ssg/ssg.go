5ede129cfdf203e27866b4dd2141e9982debaf30
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-07-09 20:13:20 +0200

    save draft

--- internal/cmd/ssg/ssg.go
+++ internal/cmd/ssg/ssg.go
@@ -1,23 +1,41 @@
 package main
+
 import (
 	"context"
 	"log"
-	"path"
 	"os"
+	"path"
 
+	"vc29/internal/layouts"
 	"vc29/internal/pages"
+
+	"github.com/a-h/templ"
 )
+
+type mainPage struct {
+	filename       string
+	templComponent templ.Component
+}
+
+var mainPages = []mainPage{
+	{"home.html", pages.Home()},
+	{"intel.html", pages.Intel()},
+	{"signals.html", pages.Signals()},
+	{"drafts.html", pages.Drafts()},
+}
+
 func main() {
 	staticPath := "static"
 	
-	name := path.Join(staticPath, "home.html")
-	f, err := os.Create(name)
+	fileHome := path.Join(staticPath, "home.html")
+	f, err := os.Create(fileHome)
 	if err != nil {
 		log.Fatalf("failed to create output file: %v", err)
 	}
 
 	// Write it out.
-	err = pages.Home().Render(context.Background(), f)
+	home := pages.Home()
+	err = layouts.Frame("VC29 | home", home).Render(context.Background(), f)
 	if err != nil {
 		log.Fatalf("failed to write index page: %v", err)
 	}
